49dcae3b21b35aa0655a4107cb8cf41834d715f8
armaanbhardwaj23 <armaanbhardwaj23@gmail.com>
2023-07-11 19:54:17 +0530

    assignment1completed

---
+++ src/app/SuccessAlert/SuccessAlert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+@Component({
+
+    selector : 'SuccessAlert',
+    templateUrl : './SuccessAlert.component.html',
+    styleUrls : ['./SuccessAlert.component.css']
+
+})
+
+export class SuccessAlert{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 import {FormsModule} from '@angular/forms';
+import { SuccessAlert } from './SuccessAlert/SuccessAlert.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    SuccessAlert
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
